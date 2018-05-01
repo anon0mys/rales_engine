3ccd3615ab01c19dcf4088a8901e5eb9e479f172
nergdvnlt <tylerlundgren@yahoo.com>
2018-04-30 18:55:22 -0600

    Working on post functionality for invoices.

--- spec/requests/api/v1/invoices_spec.rb
+++ spec/requests/api/v1/invoices_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 describe 'Invoice API' do
   it 'gives a list of invoices' do
-    create(:customer)
     test_invoices = create_list(:invoice, 5)
 
     get '/api/v1/invoices'
@@ -12,5 +11,25 @@ describe 'Invoice API' do
     invoices = JSON.parse(response.body)
 
     expect(invoices.size).to eq(test_invoices.size)
+  end
+
+  it 'gives a single invoice' do
+    test_invoice = create(:invoice)
+    id = test_invoice.id
+
+    get "/api/v1/invoices/#{id}"
+
+    expect(response).to be_success
+
+    invoice = JSON.parse(response.body)
+
+    expect(invoice["id"]).to eq(id)
+  end
+
+  it 'creates a single invoice' do
+    create_list(:customers, 2)
+    create_list(:merchants, 2)
+
+    post "/api/v1/invoices/#{id}"
   end
 end
