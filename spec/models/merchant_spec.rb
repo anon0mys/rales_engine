b5a1a75f76c8963f8e9431bca374a88e3d2c5eba
Evan Wheeler <anon0mys87@gmail.com>
2018-04-30 18:44:34 -0600

    Merchant relationships and tests complete

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 describe Merchant do
-  it { should validate_presence_of :name }
+  describe 'validations' do
+    it { should validate_presence_of :name }
+  end
+
+  describe 'relationships' do
+    it { should have_many :items }
+    it { should have_many :invoices }
+  end
 end
