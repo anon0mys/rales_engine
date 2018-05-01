9902b2c0f211603ff8708bcbc70d052a616f8167
nergdvnlt <tylerlundgren@yahoo.com>
2018-05-01 12:53:14 -0600

    Changing databse columns.

--- db/migrate/20180430214657_create_transactions.rb
+++ db/migrate/20180430214657_create_transactions.rb
@@ -1,9 +1,10 @@
 class CreateTransactions < ActiveRecord::Migration[5.1]
   def change
+    enable_extension :citext
     create_table :transactions do |t|
       t.references :invoice, foreign_key: true
       t.bigint :credit_card_number
-      t.string :result
+      t.citext :result
 
       t.timestamps
     end
