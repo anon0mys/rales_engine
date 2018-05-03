cbca8a293f3738cc686f7359462616c524472898
nergdvnlt <tylerlundgren@yahoo.com>
2018-05-03 09:49:54 -0600

    25 Items most revenue

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,12 @@ class Item < ApplicationRecord
 
   default_scope { order(:id) }
 
-  def self.most_revenue(limit)
+  def self.most_revenue(quantity)
+    unscoped
+      .select('items.*, sum(invoice_items.quantity * invoice_items.unit_price) AS revenue')
+      .joins(:invoice_items)
+      .group(:id)
+      .order('revenue DESC')
+      .limit(quantity);
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
       namespace :items do
         get '/find', to: 'find#show'
         get '/find_all', to: 'find#index'
+        get '/most_revenue', to: 'revenue#index'
       end
 
       namespace :invoice_items do
