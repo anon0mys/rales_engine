8a60d3389406e3689ab70a262989f9584f91fa93
Evan Wheeler <anon0mys87@gmail.com>
2018-05-03 09:02:56 -0600

    Transaction find_all endpoints complete

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,7 @@ Rails.application.routes.draw do
 
       namespace :transactions do
         get '/find', to: 'find#show'
+        get '/find_all', to: 'find#index'
       end
 
       resources :transactions, exept: %i[new edit]
--- spec/requests/api/v1/transactions/transaction_find_all_spec.rb
+++ spec/requests/api/v1/transactions/transaction_find_all_spec.rb
@@ -7,8 +7,8 @@ describe 'Transaction API Find controller' do
     end
 
     it 'can find transactions by result' do
-      test_transactions = create_list(:transaction, 4)
-      test_transactions << create(:transaction, result: 'success')
+      create_list(:transaction, 4)
+      create(:transaction, result: 'failure')
 
       get '/api/v1/transactions/find_all?result=success'
 
@@ -16,39 +16,39 @@ describe 'Transaction API Find controller' do
 
       transactions = JSON.parse(response.body)
 
-      expect(transactions.count).to eq(5)
+      expect(transactions.count).to eq(4)
     end
 
     it 'can find transactions by credit card number' do
-      test_transactions = create_list(:transaction, 4)
-      test_transactions << create(:transaction, credit_card_number: 123)
+      create_list(:transaction, 4)
+      create(:transaction, credit_card_number: 123)
 
-      get '/api/v1/transactions/find_all?result=success'
+      get '/api/v1/transactions/find_all?credit_card_number=123'
 
       expect(response).to be_successful
 
       transactions = JSON.parse(response.body)
 
-      expect(transactions.count).to eq(2)
+      expect(transactions.count).to eq(1)
     end
 
     it 'can find multiple transactions by invoice_id' do
-      test_transactions = create_list(:transaction, 4)
-      test_transactions << create(:transaction, invoice_id: 1)
+      create_list(:invoice, 2)
+      create_list(:transaction, 4, invoice_id: 1)
+      create_list(:transaction, 3, invoice_id: 2)
 
-      get '/api/v1/transactions/find_all?invoice_id=1'
+      get '/api/v1/transactions/find_all?invoice_id=2'
 
       expect(response).to be_successful
 
       transactions = JSON.parse(response.body)
 
-      expect(transactions.count).to eq(2)
+      expect(transactions.count).to eq(3)
     end
 
     it 'can find multiple transactions by created date' do
       create_list(:transaction, 4)
-      create(:transaction, created_at: '2018-04-30')
-      create(:transaction, created_at: '2018-04-30')
+      create_list(:transaction, 2, created_at: '2018-04-30')
 
       get '/api/v1/transactions/find_all?created_at=04-30-2018'
 
@@ -61,8 +61,7 @@ describe 'Transaction API Find controller' do
 
     it 'can find a single transaction by updated date' do
       create_list(:transaction, 4)
-      create(:transaction, updated_at: '2018-04-30')
-      create(:transaction, updated_at: '2018-04-30')
+      create_list(:transaction, 2, updated_at: '2018-04-30')
 
       get '/api/v1/transactions/find_all?updated_at=04-30-2018'
 
