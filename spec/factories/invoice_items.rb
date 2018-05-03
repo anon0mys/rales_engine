ba03ac3b769aebec54e220d0d6f6a04e793e860d
nergdvnlt <tylerlundgren@yahoo.com>
2018-05-02 21:09:37 -0600

    Fixing errors after rebasing.

--- spec/factories/invoice_items.rb
+++ spec/factories/invoice_items.rb
@@ -1,15 +1,8 @@
 FactoryBot.define do
   factory :invoice_item do
-<<<<<<< HEAD
-    invoice nil
-    item nil
-    quantity 3
-    unit_price 3
-=======
     association :invoice
     association :item
-    quantity 1
-    unit_price 1
->>>>>>> Invoice Item endpoints complete. About to do find controller.
+    quantity 3
+    unit_price 3
   end
 end
