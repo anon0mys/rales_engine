0694f821e68dd4c44f363c03db086ccaa05abdc2
nergdvnlt <tylerlundgren@yahoo.com>
2018-05-03 10:11:22 -0600

    Refactoring controller.

--- app/controllers/api/v1/items/most_revenue_controller.rb
+++ app/controllers/api/v1/items/most_revenue_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::Items::RevenueController < ApplicationController
+class Api::V1::Items::MostRevenueController < ApplicationController
   def index
     render json: Item.most_revenue(params[:quantity])
   end
